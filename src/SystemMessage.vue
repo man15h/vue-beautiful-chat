47ed95677314d1f768cd3758e429ba99d6d2183e
man15h <manish9461@gmail.com>
2018-08-15 20:58:39 +0530

    literally minor change

--- src/SystemMessage.vue
+++ src/SystemMessage.vue
@@ -8,8 +8,6 @@
 </template>
 
 <script>
-import {formatDate} from './lib';
-
 export default {
   props: {
     data: {
@@ -22,8 +20,33 @@ export default {
     }
   },
   methods:{
-    res(d){
-      formatDate(d);
+    res(data){
+      if (Number.isInteger(Number(data))) {
+        let d = new Date(data * 1000),
+        yyyy = d.getFullYear(),
+        mm = ('0' + (d.getMonth() + 1)).slice(-2),
+        dd = ('0' + d.getDate()).slice(-2),
+        hh = d.getHours(),
+        h = hh,
+        min = ('0' + d.getMinutes()).slice(-2),
+        ampm = 'AM',
+        time;
+        if (hh > 12) {
+          h = hh - 12;
+          ampm = 'PM';
+        } else if (hh === 12) {
+          h = 12;
+          ampm = 'PM';
+        } else if (hh == 0) {
+          h = 12;
+        }
+        // ie: 2013-02-18, 8:35 AM
+        time = yyyy + '-' + dd + '-' + mm + ', ' + h + ':' + min + ' ' + ampm;
+        return time;
+      }
+      else {
+        return data;
+      }
     }
   }
 }
